9592333bbf56d588cd82346a08d4806a5ac2d865
Siil777 <pavel.ivanov.thk@gmail.com>
2024-09-16 12:23:23 +0300

    feat: brackets sequence done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+          const stack = [];
+          const bracketsMap = {};
+          const openBrackets = [];
+
+          bracketsConfig.forEach(([open,close])=>{
+            bracketsMap[close]=open;
+            openBrackets.push(open);
+          });
+          for(const char of str){
+            if(openBrackets.includes(char)){
+              if(stack.length && stack[stack.length -1]===char && bracketsMap[char]===char){
+                 stack.pop();
+              }else{
+                 stack.push(char);
+              }
+            }else{
+              if(stack.length===0 || stack.pop()!==bracketsMap[char]){
+                return false;
+              }
+            }
+          }
+          return stack.length === 0;
 }
